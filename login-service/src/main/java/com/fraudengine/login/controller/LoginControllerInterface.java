034141c0dd344b8af92ffc0f80aeb65ba563366a
Ronin Naicker <roninnaicker@capitecbank.co.za>
2026-09-25 08:28:47 +0200

    remove unused import

--- login-service/src/main/java/com/fraudengine/login/controller/LoginControllerInterface.java
+++ login-service/src/main/java/com/fraudengine/login/controller/LoginControllerInterface.java
@@ -3,7 +3,6 @@ package com.fraudengine.login.controller;
 import com.fraudengine.login.service.dto.LoginRequest;
 import com.fraudengine.login.service.dto.LoginResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
